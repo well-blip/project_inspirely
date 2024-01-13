244a8c152381d3b8e3d5aaef60e23d7713863ec2
Faiz <mm2046@hw.ac.uk>
2024-01-13 16:28:32 +0400

    Files Section content and Group Chat section content added.

--- project_inspirely/src/App.tsx
+++ project_inspirely/src/App.tsx
@@ -1,11 +1,12 @@
 import { useState } from "react";
 import Spaces from "./pages/spaces";
+import GroupPage from "./components/spaces/GroupPage";
 
 function App() {
   const [count, setCount] = useState(0);
 
-  return <Spaces />;
-  // return <GroupPage />; to view GroupPage.tsx
+  // return <Spaces />;
+  return <GroupPage />; //to view GroupPage.tsx
 }
 
 export default App;
