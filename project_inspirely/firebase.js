c132f16bdc2d66799a97d87ddab5616170ff0683
Faiz <mm2046@hw.ac.uk>
2024-01-18 11:37:12 +0400

    Add Firestore initialization and collection references

--- project_inspirely/firebase.js
+++ project_inspirely/firebase.js
@@ -1,5 +1,6 @@
 import firebase from "firebase/app";
 import "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
 const app = firebase.initializeApp({
   apiKey: "AIzaSyDhX2DQRygn-KuM7VC6Xp0SmV6psZAB1cs",
@@ -9,6 +10,11 @@ const app = firebase.initializeApp({
   messagingSenderId: "541004153858",
   appId: "G-MDH4Z659K2",
 });
+
+// initialize firestore service
+const db = getFirestore();
+
+// collection references
 
 export const auth = app.auth();
 export default app;
