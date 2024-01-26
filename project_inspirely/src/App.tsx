facd624523929e5cb8d23af3c1c6fe3e2cd87ba1
Hemansih <hhb2000@hw.ac.uk>
2024-01-26 18:24:06 +0400

    commit

--- project_inspirely/src/App.tsx
+++ project_inspirely/src/App.tsx
@@ -1,12 +1,14 @@
 import { useState } from "react";
 import Spaces from "./pages/spaces";
+
 import GroupPage from "./components/spaces/GroupPage";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return <Spaces />;
-   //to view GroupPage.tsxreturn <GroupPage />;
+
+  //  return <GroupPage />;
 }
 
 export default App;
