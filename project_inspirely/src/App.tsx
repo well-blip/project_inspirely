786eb82f1efbaca1cb7d12deed23e89c2356a815
Faiz <mm2046@hw.ac.uk>
2024-03-18 18:22:34 +0400

    React router installed & Basic logic setup

--- project_inspirely/src/App.tsx
+++ project_inspirely/src/App.tsx
@@ -1,18 +1,18 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
 import GroupPage from "./components/spaces/IndividualGroupSpace/GroupPage";
+import Spaces from "./pages/spaces";
 
 function App() {
-  // const [count, setCount] = useState(0);
-  // <Routes>
-  //   <Route index element = {<Spaces/>}/>
-  //   <Route path="/spaces" element={<Spaces/>}/>
-  //   <Route path="/group" element={<GroupPage/>}/>
-  // </Routes>
-
-  // return <Spaces />;
-  return <GroupPage />;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Spaces />} />{" "}
+        <Route path="/spaces" element={<Spaces />} />
+        <Route path="/group" element={<GroupPage />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
